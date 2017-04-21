a825aaa3b17b5d3ce5c541ffe46c1dde8623c12e
Ilya-Valasiuk <Ilya_Valasiuk@epam.com>
2017-04-21 12:35:55 +0300

    add volume to mongod and fix error for connection

--- contacts/src/mongoose.js
+++ contacts/src/mongoose.js
@@ -1,6 +1,8 @@
 const config = require('config/config');
 const mongoose = require('mongoose');
 
-mongoose.connect(config.mongodb.uri);
+mongoose.connect(config.mongodb.uri, (err) => {
+    console.log(err);
+});
 
 module.exports = mongoose;
